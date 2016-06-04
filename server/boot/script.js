0a65e1c48cdbb555241a33adcf9a8b468ee27e69
elio78 <elio@canale-parola.eu>
2016-06-04 12:55:23 +0200

    Add script.js (user creation)

---
+++ server/boot/script.js
@@ -0,0 +1,31 @@
+module.exports = function(app) {
+var MongoDB = app.dataSources.db;
+
+MongoDB.automigrate('Customer', function(err) {
+   if (err) throw (err);
+   var Customer = app.models.Customer;
+
+   Customer.create([
+    {username: 'Admin', email: 'elio@canale-parola.eu', password: 'abcdef', id:'user01'},
+    {username: 'elio', email: 'elio@canale-parola.eu', password: 'Ce1mdphs', id:'user02'}
+  ], function(err, users) {
+    if (err) return cb(err);
+     var Role = app.models.Role;
+    var RoleMapping = app.models.RoleMapping;
+
+    //create the admin role
+    Role.create({
+      name: 'admin'
+    }, function(err, role) {
+      if (err) cb(err);
+       //make admin
+      role.principals.create({
+        principalType: RoleMapping.USER,
+        principalId: users[0].id
+      }, function(err, principal) {
+        if (err) throw (err);
+      });
+    });
+  });
+});
+};
